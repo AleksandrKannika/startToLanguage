eaab35e2ed63b5563fefab172bafa5864e7c8c36
Kannika Aleksandr <car113man@gmail.com>
2022-06-27 04:15:53 +0300

    add new int

--- Ex005/Program.cs
+++ Ex005/Program.cs
@@ -2,4 +2,5 @@
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
-Console.WriteLine(numberA + numberB);
+int summ = numberA + numberB;
+Console.WriteLine(summ);
