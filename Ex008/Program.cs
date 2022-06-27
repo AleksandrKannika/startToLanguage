6d6c68bfacf744db2ec8330f9fc4f2b5393e2a5c
Kannika Aleksandr <car113man@gmail.com>
2022-06-27 06:18:12 +0300

    Fix bug

---
+++ Ex008/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
+int xa = 50, ya = 1,
+    xb = 1, yb = 25,
+    xc = 100, yc = 25;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
