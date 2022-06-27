e75e8b419afea8cd2e1de22414aee5f79a07de9d
Kannika Aleksandr <car113man@gmail.com>
2022-06-27 05:21:29 +0300

    Hi, big boss

---
+++ Ex006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name - ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "aleksandr")
+{
+    Console.WriteLine("Oh, big boss Alexandr!");
+}
+else
+{
+    Console.Write("hi, ");
+    Console.Write(userName);
+}
