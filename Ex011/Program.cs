13190c4be26b38b159c96bc627e17b34f9aadc2e
Kannika Aleksandr <car113man@gmail.com>
2022-07-05 00:54:02 +0300

    method array

---
+++ Ex011/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 23, 42, 34, 12, 43, 12, 67, 81 };
+int n = array.Length;
+int find = 12;
+int index = 0;
+while (index < n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
